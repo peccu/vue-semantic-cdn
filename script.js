8dfd57181de5956e4891ae4261988721c02ca9e4
peccu <peccul@gmail.com>
2017-11-29 20:28:49 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var app = new Vue({
+  el: '#app',
+  props: [
+  ],
+  components: {
+  },
+  computed: {
+  },
+  data: {
+  },
+  watch: {
+  },
+  beforeCreate: function(){
+  },
+  created: function(){
+  },
+  beforeMount: function(){
+  },
+  mounted: function(){
+  },
+  beforeUpdate: function(){
+  },
+  updated: function(){
+  },
+  beforeDestroy: function(){
+  },
+  destroyed: function(){
+  },
+  methods: {
+  }
+});
